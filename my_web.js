dee1997a1a090c74d305cdc8776e946ad77d16be
mohit <mohit@startflux.com>
2017-09-13 12:43:58 +0530

    first commit

---
+++ my_web.js
@@ -0,0 +1,12 @@
+  var VLogin = angular.module('myapp',[]);
+
+VLogin.controller('myController', ['$scope',function($scope) {
+
+    $scope.clicked = function(){  
+        alert(0);
+
+     window.location('https://bootstrapbay.com/blog/built-with-bootstrap/');
+    }
+
+}]);
+            
